69de1b1d76e5ab10a63a86ad49e2fa5d7ad08e04
Christopher Lamm <chris@theantichris.com>
2020-11-11 12:20:27 -0500

    main function

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
 	gorm.io/driver/postgres v1.0.5
 	gorm.io/gorm v1.20.6
 )
--- main.go
+++ main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	args := Args{
+		conn: os.Getenv("DB"),
+		port: os.Getenv("PORT"),
+	}
+
+	if err := Run(args); err != nil {
+		log.Println(err)
+	}
+}
